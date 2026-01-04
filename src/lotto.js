e69d5452302ef59bb055ab41923a42037ac5bbf3
anthonymin <dbstnsdl12@naver.com>
2026-01-04 23:01:08 +0900

    fix: 로또 당첨번호 수집 스크립트 수정 - 동행복권 웹사이트 리뉴얼 대응

--- src/lotto.js
+++ src/lotto.js
@@ -1,8 +1,10 @@
 import { parse } from "node-html-parser";
 import { Octokit } from "@octokit/rest";
 import stringify from "json-stringify-pretty-compact";
-import { format } from "date-fns";
+import { format, parse as parseDate } from "date-fns";
 import dotenv from "dotenv";
+import { chromium as pw } from "playwright-core";
+import chromium from "@sparticuz/chromium";
 
 dotenv.config();
 
@@ -10,137 +12,91 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_ACCESS_TOKEN,
   timeZone: "Asia/Seoul",
 });
-
-const wait = (amount = 0) =>
-  new Promise((resolve) => setTimeout(resolve, amount));
-
 async function handler() {
+  let browser = null;
   try {
-    const owner = "anthonyminyungi";
-    const ownerEmail = "dbstnsdl12@naver.com";
-    const repo = "rantto";
-    const filePath = "src/assets/winning_history.json";
-    const message = "Update winning_history.json";
-    const main = "main";
-    const tempBranch = "functions/update-winning-history";
-    const dhlotteryUrl = "https://dhlottery.co.kr/gameResult.do?method=byWin";
+    const LOTTO_RESULT_API_BASE_URL =
+      "http://www.dhlottery.co.kr/lt645/selectPstLt645Info.do";
+    const FILE_NAME = "lotto-winning-history.json";
+    const GIST_ID = "a7237c0717400512855c890d5b0e1ba3";
+
+    /* gist에서 json 파일 접근 */
+    const { data: gist } = await octokit.gists.get({
+      gist_id: GIST_ID,
+    });
+
+    const gistContent = gist.files[FILE_NAME]?.content;
+    const historyJson = JSON.parse(gistContent);
+    const prevRound = historyJson.history.at(-1).round;
 
     /* 이번주 당첨번호 크롤링 */
-    const data = await fetch(dhlotteryUrl);
-    const root = parse(await data.text());
-    const numbers = root
-      .querySelectorAll(".nums .win .ball_645")
-      .map((elem) => Number.parseInt(elem.text, 10));
-    const bonus = Number.parseInt(
-      root.querySelector(".nums .bonus .ball_645")?.text || "",
-      10
-    );
-    const round = Number.parseInt(
-      (root.querySelector(".win_result strong")?.text || "").slice(0, 4),
-      10
-    );
-    console.log("data : ", round, numbers, bonus);
-
-    /* 메인 헤드 접근 */
-    const { data: getRef } = await octokit.git.getRef({
-      owner,
-      repo,
-      ref: `heads/${main}`,
+    console.log("Launching browser...");
+    browser = await pw.launch({
+      headless: true,
+      executablePath: await chromium.executablePath(),
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      ignoreHTTPSErrors: true,
     });
-    const mainSha = getRef.object.sha;
-    console.log(mainSha);
 
-    await wait(2000);
+    console.log("Browser launched");
+    const context = await browser.newContext();
+    const page = await context.newPage();
 
-    /* 임시 브랜치 생성 */
-    const isTempRefExist = await octokit.rest.git
-      .getRef({
-        owner,
-        repo,
-        ref: `refs/heads/${tempBranch}`,
-      })
-      .catch(() => false);
-    if (!isTempRefExist) {
-      const { data: createRef } = await octokit.rest.git.createRef({
-        owner,
-        repo,
-        ref: `refs/heads/${tempBranch}`,
-        sha: mainSha,
-      });
-      console.log("create ref : ", createRef);
-
-      await wait(2000);
-    }
-
-    /* 기존 당첨번호 데이터 접근 */
-    const { data: getContent } = await octokit.repos.getContent({
-      owner,
-      repo,
-      path: filePath,
-      ref: `refs/heads/${tempBranch}`,
-    });
-    let json;
-    let getContentSha = "";
-    if (!Array.isArray(getContent) && getContent.type === "file") {
-      json = JSON.parse(atob(getContent.content));
-      getContentSha = getContent.sha;
-    }
-    /* 이번주 당첨번호 추가 */
-    json.history.push({
-      round,
-      bonus,
-      numbers,
-      createdAt: format(new Date(), "yyyy-MM-dd"),
-    });
-    console.log("json : ", json);
-
-    await wait(2000);
-
-    /* 임시 브랜치에 추가된 데이터 반영 update commit */
-    const { data: updateFile } = await octokit.repos.createOrUpdateFileContents(
+    console.log("Navigating...");
+    await page.goto(
+      `${LOTTO_RESULT_API_BASE_URL}?srchLtEpsd=${prevRound + 1}`,
       {
-        owner,
-        repo,
-        branch: tempBranch,
-        path: filePath,
-        message,
-        content: btoa(stringify(json)),
-        sha: getContentSha,
-        committer: {
-          name: owner,
-          email: ownerEmail,
-        },
+        waitUntil: "domcontentloaded",
+        timeout: 60000,
       }
     );
-    console.log("commit id : ", updateFile.commit.sha);
 
-    // waiting for completion of preview deployment
-    await wait(40000);
+    console.log("Getting content...");
+    const content = await page.content();
+    console.log(content);
 
-    /* 임시 브랜치 -> main pull request 생성 */
-    const { data: createPR } = await octokit.rest.pulls.create({
-      owner,
-      repo,
-      head: tempBranch,
-      base: main,
-      title: message,
+    const root = parse(content);
+    const { data } = JSON.parse(root.querySelector("pre").textContent);
+    const {
+      ltEpsd,
+      ltRflYmd,
+      tm1WnNo,
+      tm2WnNo,
+      tm3WnNo,
+      tm4WnNo,
+      tm5WnNo,
+      tm6WnNo,
+      bnsWnNo,
+    } = data.list[0];
+
+    /* 획득한 json 데이터에 최신 당첨번호 추가 */
+    historyJson.history.push({
+      bonus: bnsWnNo,
+      round: ltEpsd,
+      numbers: [tm1WnNo, tm2WnNo, tm3WnNo, tm4WnNo, tm5WnNo, tm6WnNo],
+      createdAt: format(
+        parseDate(ltRflYmd, "yyyyMMdd", new Date()),
+        "yyyy-MM-dd"
+      ),
     });
-    console.log("pr number : ", createPR.number);
+    console.log("updated json : ", historyJson.history.at(-1));
 
-    await wait(2000);
-
-    /* 생성된 pull request 병합 */
-    const { data: mergePR } = await octokit.rest.pulls.merge({
-      owner,
-      repo,
-      pull_number: createPR.number,
+    /* gist에 업데이트한 json 파일 반영 */
+    console.log("Updating gist...");
+    await octokit.gists.update({
+      gist_id: GIST_ID,
+      files: {
+        [FILE_NAME]: {
+          content: stringify(historyJson),
+        },
+      },
     });
-    console.log("merged : ", mergePR.merged);
-    if (!mergePR.merged) {
-      throw new Error("pull request not merged");
-    }
+    console.log("Gist successfully updated.");
   } catch (e) {
     console.error(e);
+  } finally {
+    await browser?.close();
   }
 }
 
